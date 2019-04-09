e3c375a60d98192646510aec5505d4b45db09502
Noah <noaholatoye@gmail.com>
2019-04-09 07:53:33 +0100

    add if statement to the linechart

--- js/chart.js
+++ js/chart.js
@@ -2,6 +2,9 @@
 
 let webTrafficChart = document.getElementById("web-traffic").getContext("2d");
 let btnFdi = document.getElementById("btn-fdi");
+let btnPortfolio = document.getElementById("btn-portfolio");
+let btnOther = document.getElementById("btn-other");
+let btnCapital = document.getElementById("btn-capital");
 
 /* ==============================
 Declare data for line chart
@@ -122,7 +125,52 @@ let dataCapital = [
 ];
 
 // Display chart
-var myLineChart = new Chart(webTrafficChart, {
+// var myLineChart = new Chart(webTrafficChart, {
+// 	responsive: true,
+// 	type: "line",
+// 	data: {
+// 		labels: labelTable,
+// 		datasets: [
+// 			{
+// 				label: labelFdi,
+// 				data: dataFdi,
+// 				backgroundColor: "rgba(77, 75, 113, 0.2)",
+// 				pointBackgroundColor: "#fff",
+// 				pointBorderColor: "#4d4b71"
+// 			},
+// 			{
+// 				label: labelPortfolio,
+// 				data: dataPortfolio,
+// 				backgroundColor: "rgba(179, 179, 179, 0.3)",
+// 				pointBackgroundColor: "#fff",
+// 				pointBorderColor: "#b3b3b3"
+// 			},
+// 			{
+// 				label: labelOther,
+// 				data: dataOther,
+// 				backgroundColor: "rgba(130, 201, 144, 0.3)",
+// 				pointBackgroundColor: "#fff",
+// 				pointBorderColor: "#81c98f"
+// 			},
+// 			{
+// 				label: labelCapital,
+// 				data: dataCapital,
+// 				backgroundColor: "rgba(115, 118, 191, 0.3)",
+// 				pointBackgroundColor: "#fff",
+// 				pointBorderColor: "#7376bf"
+// 			}
+// 		]
+// 	},
+// 	options: {
+// 		legend: {
+// 			display: false
+// 		}
+// 	}
+// });
+
+// Toggle Line Chart
+
+let myLineChart = new Chart(webTrafficChart, {
 	responsive: true,
 	type: "line",
 	data: {
@@ -134,46 +182,56 @@ var myLineChart = new Chart(webTrafficChart, {
 				backgroundColor: "rgba(77, 75, 113, 0.2)",
 				pointBackgroundColor: "#fff",
 				pointBorderColor: "#4d4b71"
-			},
-			{
-				label: labelPortfolio,
-				data: dataPortfolio,
-				backgroundColor: "rgba(179, 179, 179, 0.3)",
-				pointBackgroundColor: "#fff",
-				pointBorderColor: "#b3b3b3"
-			},
-			{
-				label: labelOther,
-				data: dataOther,
-				backgroundColor: "rgba(130, 201, 144, 0.3)",
-				pointBackgroundColor: "#fff",
-				pointBorderColor: "#81c98f"
-			},
-			{
-				label: labelCapital,
-				data: dataCapital,
-				backgroundColor: "rgba(115, 118, 191, 0.3)",
-				pointBackgroundColor: "#fff",
-				pointBorderColor: "#7376bf"
 			}
 		]
 	},
 	options: {
 		legend: {
 			display: false
+		},
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: false
+					}
+				}
+			],
+			xAxes: [
+				{
+					ticks: {
+						beginAtZero: false
+					}
+				}
+			]
 		}
 	}
 });
 
-// Toggle Line Chart
+function changeChart(chart, labels, data) {
+	chart.data.labels = labels;
+	chart.data.datasets.forEach(dataset => {
+		dataset.data = data;
+	});
+	chart.update();
+}
 
-// const myFdiButton = (chart, labels, data) => {
-// 	chart.data.labels = labels;
-// 	chart.data.datasets.forEach(dataset => {
-// 		dataset.data = data;
-// 	});
-// 	chart.update();
-// 	console.log(chart.update());
-// };
+function clickToggle(element) {
+	if (element == btnFdi) {
+		changeChart(myLineChart, labelFdi, dataFdi);
+	}
+	if (element == btnOther) {
+		changeChart(myLineChart, labelOther, dataOther);
+	}
+	if (element == btnCapital) {
+		changeChart(myLineChart, labelCapital, dataCapital);
+	}
+	if (element == btnPortfolio) {
+		changeChart(myLineChart, labelPortfolio, dataPortfolio);
+	}
+}
 
-// console.log(data);
+clickToggle(btnFdi);
+clickToggle(btnOther);
+clickToggle(btnCapital);
+clickToggle(btnPortfolio);
