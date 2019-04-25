5f5e7afed9d6e38fc4ca17720a8f03a3b79eb3c2
Noah Olatoye <noaholatoye@gmail.com>
2019-04-25 19:37:16 +0100

    will add all functionalities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// "use strict";
+// // Test for local storage
+// function supportsLocalStorage() {
+// 	try {
+// 		return "localStorage" in window && window["localStorage"] !== null;
+// 	} catch (e) {
+// 		return false;
+// 	}
+// }
+
 let closeWindow = document.querySelector(".fa-window-close");
 
 // Hamburger menu
@@ -17,7 +27,6 @@ window.onscroll = function() {
 
 let navbar = document.getElementById("navbar");
 let sticky = navbar.offsetTop;
-console.log(sticky.node);
 
 function myStickyMenu() {
 	if (window.pageYOffset >= sticky) {
@@ -27,18 +36,15 @@ function myStickyMenu() {
 	}
 }
 
+// Close alert box
+let alertBox = document.querySelector(".alert");
+let closeAlert = document.querySelector(".fa-window-close");
+let removeAlert = () => {
+	alertBox.style.display = "none";
+};
+
 // Notifications
 var el = document.querySelector(".fa-bell");
-
-// var count = Number(el.getAttribute('data-count')) || 0;
-// el.setAttribute('data-count', count + 1);
-// el.classList.remove('notify');
-// el.offsetWidth = el.offsetWidth;
-// el.classList.add('notify');
-// if(count === 0){
-//     el.classList.add('show-count');
-// };
-
 let getNotifications = dataCount => {
 	let data = dataCount;
 	var count = Number(el.getAttribute("data-count")) || 0;
@@ -53,3 +59,100 @@ let getNotifications = dataCount => {
 };
 
 getNotifications(2);
+
+// Display notification
+let myNotifications = () => {
+	let notificationBox = document.querySelector(".notification-box");
+	notificationBox.classList.toggle("display-none");
+};
+
+// Autocompletion to the search field
+$(function() {
+	var availableTags = [
+		"Victoria Chambers",
+		"Dale Byrd",
+		"Dawn Wood",
+		"Dan Oliver"
+	];
+	$("#search-user").autocomplete({
+		source: availableTags
+	});
+});
+
+// =====================================
+//  localStorage
+// =====================================
+
+// Validate and save strings to store of past searches
+// function saveSearchString(str) {
+// 	var searches = getRecentSearches();
+// 	if (searches.indexOf(str) > -1 || !str) {
+// 		return false;
+// 	}
+// 	searches.push(str);
+// 	localStorage.setItem("recentSearches", JSON.stringify(searches));
+// 	return true;
+// }
+
+// window.onload = function() {
+// 	// Make sure Local Storage exists before trying to use it
+// 	if (supportsLocalStorage()) {
+// 		// Get references to DOM elements
+// 		var toggle1 = document.getElementById("toggle--off--on1");
+// 		var toggle2 = document.getElementById("toggle--off--on2");
+// 		var timeZone = document.getElementById("timezone");
+// 		var saveSetting = document.querySelector("save");
+
+// 		// Set event handlers
+// 		saveSetting.addEventListener("submit", function(event) {
+// 			// Store
+// 			let saveLocal = localStorage.setItem(toggle1, toggle2, timeZone);
+// 			if (saveSearchString(saveLocal)) {
+// 			}
+// 		});
+// 	}
+// };
+
+// Save Settings to Local Storage
+
+const emailSettings = document.getElementById("toggle--off--on1");
+const profileSettings = document.getElementById("toggle--off--on2");
+const timeZone = document.getElementById("timezone");
+const setEmail = localStorage.getItem("emailSettings");
+const setProfile = localStorage.getItem("profileSettings");
+const setTimeZone = localStorage.getItem("timeZone");
+const saveBtn = document.getElementById("save-settings");
+const cancelBtn = document.getElementById("cancel-settings");
+
+const storeData = function() {
+	if (setEmail !== null) {
+		emailSettings.checked = setEmail === "true";
+	}
+
+	if (setProfile !== null) {
+		profileSettings.checked = setProfile === "true";
+	}
+
+	if (setTimeZone !== null) {
+		timeZone.value = setTimeZone;
+	}
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	saveBtn.addEventListener("click", function() {
+		localStorage.setItem("emailSettings", emailSettings.checked);
+		localStorage.setItem("profileSettings", profileSettings.checked);
+		localStorage.setItem("timeZone", timeZone.value);
+		alert("Great! Your settings have been saved");
+	});
+
+	cancelBtn.addEventListener("click", function() {
+		const cancel = confirm("Continue without saving?");
+
+		if (emailSettings == true) {
+			localStorage.removeItem = emailSettings;
+		}
+	});
+
+	storeData();
+});
